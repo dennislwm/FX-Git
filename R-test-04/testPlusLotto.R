fad684b7978f604e13695b3198f1b0f0d2f8d6ce
Dennis Lee <dennislwm@yahoo.com.au>
2012-11-24 15:38:58 +0800

    1.0.0   This script perform unit tests on functions in PlusLotto.R.

---
+++ R-test-04/testPlusLotto.R
@@ -0,0 +1,75 @@
+#|------------------------------------------------------------------------------------------|
+#|                                                                          testPlusLotto.R |
+#|                                                             Copyright © 2012, Dennis Lee |
+#|                                                                                          |
+#| Assert History                                                                           |
+#|  1.0.0   This script perform unit tests on functions in PlusPdf.R.                       |
+#|------------------------------------------------------------------------------------------|
+source("C:/Users/denbrige/100 FXOption/103 FXOptionVerBack/080 FX Git/R-source/PlusLotto.R")
+
+context("PlusLotto checks")
+
+test_that( "lottoArimaConfDfr",
+{
+  #---  Assert THREE (3) arguments:                                                   
+  #       rawMtx:       a numeric matrix with at least ONE (1) column to be forecasted
+  #       lNum:         a numeric vector with lower bounds                 
+  #       uNum:         a numeric vector with upper bounds (default: NULL)                 
+  
+  #---  Backup user environment for restore
+  user.wd <- getwd()
+  
+  #---  Set working environment
+  #       Backup user files
+  test.wd <- paste0(RegGetRDir(),"R-test-04/")
+  setwd(test.wd)
+  
+  #---  Test without any data
+  #       (1) rawMtx is NULL or EMPTY
+  testMtx <- matrix(numeric(0), ncol=0, nrow=0)
+  expect_that( lottoArimaConfDfr(NULL, 0), throws_error() )
+  expect_that( lottoArimaConfDfr(testMtx, 0), throws_error() )
+  
+  #       (3) lNum is NULL or EMPTY
+  #       (5) uNum is EMPTY
+  tes2Mtx <- matrix(c(10,20,5), ncol=1, nrow=3)
+  expect_that( lottoArimaConfDfr(tes2Mtx, NULL), throws_error() )
+  expect_that( lottoArimaConfDfr(tes2Mtx, numeric(0)), throws_error() )
+  expect_that( lottoArimaConfDfr(tes2Mtx, 1, numeric(0)), throws_error() )
+  
+  #---  Test with incorrect uni data
+  #       (7) rawMtx has ONE (1) column and THREE (3) rows of NA
+  tes3Mtx <- matrix(c(NA,NA,NA), ncol=1, nrow=3)
+  expect_that( lottoArimaConfDfr(tes3Mtx, 1), throws_error() )
+  
+  #       (8) rawMtx has ONE (1) column and THREE (3) row of characters
+  tes4Mtx <- matrix(c("a","b","c"), ncol=1, nrow=3)
+  expect_that( lottoArimaConfDfr(tes4Mtx, 1), throws_error() )
+  
+  #       (9) lNum is negative
+  #       (10) lNum is greater than length ONE (1) but not EQUAL to number of columns 
+  #            in the matrix
+  #       (11) uNum is negative
+  #       (12) uNum is less than lNum
+  #       (13) uNum is greater than length ONE (1) but not EQUAL to number of columns 
+  #            in the matrix
+  expect_that( lottoArimaConfDfr(tes2Mtx, -1), is_a("data.frame") )
+  expect_that( lottoArimaConfDfr(tes2Mtx, c(-1,-1)), throws_error() )
+  expect_that( nrow(lottoArimaConfDfr(tes2Mtx, -1, -1)), is_equivalent_to(2) )
+  expect_that( lottoArimaConfDfr(tes2Mtx, 1, -40), throws_error() )
+  expect_that( lottoArimaConfDfr(tes2Mtx, 1, c(-1,-1)), throws_error() )
+  
+  #---  Test with correct uni data
+  #       (14) rawMtx has ONE (1) column and THREE (3) rows of numeric data
+  expect_that( lottoArimaConfDfr(tes2Mtx, 1, 36), is_a("data.frame") )
+  
+  #---  Test with correct multi data
+  #       (15) rawMtx has multiple columns and rows of numeric data
+  tes5Mtx <- matrix(c(10,20,5,14,25,34), ncol=2, nrow=3)
+  expect_that( nrow(lottoArimaConfDfr(tes5Mtx, 0, 36)), is_equivalent_to(3) )
+  
+  #---  Restore user environment
+  #       Restore user files
+  setwd(user.wd)
+}
+)
