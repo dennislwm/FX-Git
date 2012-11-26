b474462ec231ee3d72d19ae60c4f8f2ccb4e43cf
Dennis Lee <dennislwm@yahoo.com.au>
2012-11-26 17:47:29 +0800

    1.0.1   Major bug fixes including interaction flow, output variables and reactiveUI.                Incorporated Roulette (r) into both server and ui.

--- R-shiny-01/server.R
+++ R-shiny-01/server.R
@@ -24,60 +24,271 @@
 #|  Note: To kill a process, type 'ps' to search for the pid, and then type 'kill -9 <pid>'.|
 #|                                                                                          |
 #| Assert History                                                                           |
+#|  1.0.1   Major bug fixes including interaction flow, output variables and reactiveUI.    |
+#|            Incorporated Roulette (r) into both server and ui.                            | 
 #|  1.0.0   This script contains the shinyServer() function for PlusLotto.R.                |
 #|------------------------------------------------------------------------------------------|
 library(shiny)
-source("~/100 FxOption/103 FxOptionVerBack/080 Fx Git/R-source/PlusFile.R")
 source("~/100 FxOption/103 FxOptionVerBack/080 Fx Git/R-source/PlusLotto.R")
 
-myServer <- function(input, output) {
+myServer <- function(input, output) 
+{
+  #---  The interaction flow
+  #       (1) UI disabled, wait for database to update
+  #         (i)   Database updated
+  #       (2) Click Ok, UI enabled for Lotto
+  #         (ii)  Arima Summary loaded
+  #       (3) Click Ok, UI enabled for Tickets, and UI enabled for System
+  #           (r: UI enabled for Number)
+  #         (iii) Arima Confidence loaded, Lotto Result loaded
+  #       (4) Click Ok
+  #         (iv)  Lotto Tickets loaded
   
-  #---  timestamp last updated
-  gTimeStamp <- attr(myServer, "gTimeStamp")
-  if( is.null(gTimeStamp) ) {
-    #---  init timestamp to yesterday at 00:00
-    yNum <- as.POSIXlt(Sys.Date()-1)$year+1900
-    mNum <- as.POSIXlt(Sys.Date()-1)$mon+1
-    dNum <- as.POSIXlt(Sys.Date()-1)$mday
-    gTimeStamp <- ISOdatetime(yNum,mNum,dNum,0,0,0)
-  }
-  if( lottoUpdateBln(gTimeStamp) )
-  {
+  #---  Output variables
+  #       (1) update.FUN() returns a number
+  #       (2) output$lotto.selectInput is a UI
+  #       (3) output$ticket.selectInput is a UI
+  #       (4) output$system.selectInput is a UI
+  #       (5) output$number.selectInput is a UI
+  #       (6) output$summaryTxt is a Print
+  #       (7) output$intervalTxt is a Print
+  update.FUN <- reactive(function() {
     tmp.wd <- getwd()
-    LottoUpdate()
-    attr(myServer, "gTimeStamp") <<- Sys.time()
+    retNum <- LottoUpdateNum()
     setwd(tmp.wd)
-  }
-  
-  # Generate a summary of the dataset
-  output$summaryTxt <- reactivePrint(function() {
-    LottoArimaSummary()
-  })
-  
-  output$resultTxt <- reactivePrint(function() {
-    resNul <- LottoResult( input$lottoStr )
-  })
-  
-  output$intervalTxt <- reactivePrint( function() {
-    LottoArimaConf( input$lottoStr )
-  })
-  
-  output$ticketTxt <- reactivePrint( function() {
-    if( input$systemChr == "NA" | input$lottoStr == "4d" )
-      Lotto( input$lottoStr, input$ticketNum )
+    retNum
+  } )  
+  #       (2) output$lotto.selectInput is a UI
+  output$lotto.selectInput <- reactiveUI(function() {
+    if( is.numeric(update.FUN()) )
+      selectInput( "lottoStr", "Choose a Lotto game:",
+                   choices=c("powerball", "ozlotto", "satlotto", "wedlotto",
+                             "toto", "4d", "r") )
     else
+      NULL
+  } )
+  #       (3) output$ticket.selectInput is a UI
+  output$ticket.selectInput <- reactiveUI(function() {
+    if( length(input$lottoStr)>0 )
     {
-      systemNum <- as.numeric(input$systemChr)
-      if( systemNum > 7 )
-        LottoSystem( systemNum, input$lottoStr, input$ticketNum )
+      if( input$lottoStr != "r" )
+        selectInput( "ticketNum", "Number of tickets to generate:",
+                     choices=c(12,13,14,15,16,17,18,19,20) )
       else
-      {
-        if( input$lottoStr == "ozlotto" | input$lottoStr == "toto" )
-          Lotto( input$lottoStr, input$ticketNum )
-        else
-          LottoSystem( systemNum, input$lottoStr, input$ticketNum )
-      }
+        selectInput( "ticketNum", "Number of tickets to generate:",
+                     choices=c(1,2,3,4,5,6) )
     }
+    else
+      NULL
+  } )
+  #       (4) output$system.selectInput is a UI
+  output$system.selectInput <- reactiveUI(function() {
+    if( length(input$lottoStr)>0 )
+    {
+      if( input$lottoStr != "4d" & input$lottoStr != "r" )
+        selectInput( "systemChr", "Choose a System:",
+                     choices=c("NA", "7", "8", "9", "10", "11", "12") )
+      else
+        NULL
+    }
+    else
+      NULL
+  } )
+  #       (5) output$number.selectInput is a UI
+  output$number.selectInput <- reactiveUI(function() {
+    if( length(input$lottoStr)>0 )
+    {
+      if( input$lottoStr == "r" )
+        selectInput( "randChr", "Choose a r number:",
+                     choices=c("Refresh", "Start Over", 
+                               "0","1","2","3","4","5","6","7","8","9",
+                               "10","11","12","13","14","15","16","17","18","19",
+                               "20","21","22","23","24","25","26","27","28","29",
+                               "30","31","32","33","34","35","36") )
+      else
+        NULL
+    }
+    else
+      NULL
+  } )
+  #       (6) output$summaryTxt is a Print
+  output$summaryTxt <- reactivePrint(function() {
+    if( is.numeric(update.FUN()) )
+      LottoArimaSummary()
+    else
+      "Select a Lotto game."
+  })
+  r.FUN <- reactive(function() {
+    if( length(input$lottoStr)>0 )
+    {
+      if( input$lottoStr == "r" )
+      {
+        if( length(input$randChr)>0 )
+        {
+          #---  r file is updated
+          #       (1) user wants to start over
+          randFileChr <- paste0(RegGetRNonSourceDir(),"r.csv")
+          if( input$randChr == "Start Over" ) 
+            NULL
+          else
+          {
+            randDfr <- fileReadDfr( input$lottoStr )
+            randDfr
+          }
+        }
+        else NULL
+      }
+      else NULL
+    }
+    else NULL
+  })
+  #       (7) output$intervalTxt is a Print
+  output$intervalTxt <- reactivePrint( function() {
+    if( length(input$lottoStr)>0 )
+    {
+      if( input$lottoStr == "r" )
+      {
+        if( length(input$randChr)>0 )
+        {
+          #---  r file is updated
+          #       (1) user wants to start over
+          #       (2) Arima Confidence Interval with update
+          #       (3) Arima Confidence Interval without update
+          randFileChr <- paste0(RegGetRNonSourceDir(),"r.csv")
+          if( input$randChr == "Start Over" )
+          {
+            #   (1) user wants to start over
+            if( file.exists(randFileChr) ) file.remove(randFileChr)
+            "Start Over"
+          }
+          else if( input$randChr == "Refresh" )
+          {
+            #   (4) Arima Confidence Interval without update
+            randDfr <- fileReadDfr( input$lottoStr )
+            if( is.null(randDfr) ) retNum <- 0
+            else retNum <- nrow(randDfr)
+            if( retNum>=3 )
+            {
+              confDfr <- LottoArimaConf( input$lottoStr )
+              confDfr[1,]
+            }
+            else
+              "R contains LESS THAN THREE (3) numbers"
+          }
+          else
+          {
+            randNum <- as.numeric(input$randChr)
+            retNum <- lottoRandUpdateNum(randNum)
+            #   (3) Arima Confidence Interval with update
+            if( retNum>=3 )
+            {
+              confDfr <- LottoArimaConf( input$lottoStr )
+              confDfr[1,]
+            }
+            else
+              "R contains LESS THAN THREE (3) numbers"
+          }
+        }
+        else
+          "Select a R number."
+      }
+      else
+        LottoArimaConf( input$lottoStr )
+    }
+    else
+      "Select a Lotto game."
+  })
+  output$resultTxt <- reactivePrint(function() {
+    if( length(input$lottoStr)>0 )
+    {
+      if( input$lottoStr == "r" )
+      {
+        if( length(input$randChr)>0 )
+        {
+          if( input$randChr == "Start Over" )
+            "Start Over"
+          else
+            LottoResult( input$lottoStr, resNum=99 )
+        }
+        else
+          LottoResult( input$lottoStr, resNum=99 )
+      }
+      else
+        LottoResult( input$lottoStr )
+    }
+    else
+      "Select a Lotto game."
+  })
+  output$ticketTxt <- reactivePrint( function() {
+    if( length(input$lottoStr)>0 )
+    {
+      if( length(input$ticketNum)>0 )
+      {
+        if( input$lottoStr == "r" )
+        {
+          if( length(input$randChr)>0 )
+          {
+            if( input$randChr == "Start Over" )
+              "Start Over"
+            else
+            {
+              rDfr <- r.FUN()
+              if( !is.null(rDfr) )
+              {
+                if( nrow(rDfr)>=3 )
+                  Lotto( input$lottoStr, as.numeric(input$ticketNum) )
+                else
+                  "R contains LESS THAN THREE (3) numbers"
+              }
+              else
+                "R contains LESS THAN THREE (3) numbers"
+            }
+          }
+          else  
+          {
+            rDfr <- r.FUN()
+            if( !is.null(rDfr) )
+            {
+              if( nrow(rDfr)>=3 )
+                Lotto( input$lottoStr, as.numeric(input$ticketNum) )
+              else
+                "R contains LESS THAN THREE (3) numbers"
+            }
+            else
+              "R contains LESS THAN THREE (3) numbers"
+          }
+        }
+        else if( input$lottoStr == "4d" )
+          Lotto( input$lottoStr, as.numeric(input$ticketNum) )
+        else
+        {
+          if( length(input$systemChr)>0 )
+          {
+            if( input$systemChr != "NA" )
+            {
+              systemNum <- as.numeric(input$systemChr)
+              if( systemNum > 7 )
+                LottoSystem( systemNum, input$lottoStr, as.numeric(input$ticketNum) )
+              else
+              {
+                if( input$lottoStr == "ozlotto" | input$lottoStr == "toto" )
+                  Lotto( input$lottoStr, as.numeric(input$ticketNum) )
+                else
+                  LottoSystem( systemNum, input$lottoStr, as.numeric(input$ticketNum) )
+              }
+            }
+            else
+              Lotto( input$lottoStr, as.numeric(input$ticketNum) )
+          }
+          else
+            Lotto( input$lottoStr, as.numeric(input$ticketNum) )
+        }
+      }
+      else
+        "Select a Lotto game and no. of Tickets."
+    }
+    else
+      "Select a Lotto game and no. of Tickets."
   })
 }
 
