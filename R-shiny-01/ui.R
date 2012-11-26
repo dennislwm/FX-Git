9d6e76cbe921bbf6b9d87458733eb50c8500c156
Dennis Lee <dennislwm@yahoo.com.au>
2012-11-26 17:47:02 +0800

    1.0.1   Major bug fixes including interaction flow, output variables and reactiveUI.                Incorporated Roulette (r) into both server and ui.

--- R-shiny-01/ui.R
+++ R-shiny-01/ui.R
@@ -3,10 +3,11 @@
 #|                                                             Copyright © 2012, Dennis Lee |
 #|                                                                                          |
 #| Assert History                                                                           |
+#|  1.0.1   Major bug fixes including interaction flow, output variables and reactiveUI.    |
+#|            Incorporated Roulette (r) into both server and ui.                            | 
 #|  1.0.0   This script contains the shinyUI() function for PlusLotto.R.                    |
 #|------------------------------------------------------------------------------------------|
 library(shiny)
-source("~/100 FxOption/103 FxOptionVerBack/080 Fx Git/R-source/PlusFile.R")
 source("~/100 FxOption/103 FxOptionVerBack/080 Fx Git/R-source/PlusLotto.R")
 
 # Define UI for dataset viewer application
@@ -18,14 +19,18 @@ shinyUI(pageWithSidebar(
   # Sidebar with controls to select a dataset and specify the number
   # of observations to view
   sidebarPanel(
-    selectInput( "lottoStr", "Choose a Lotto game:",
-                 choices=c("powerball", "ozlotto", "satlotto", "wedlotto", "toto", "4d") ),
+    conditionalPanel(
+      condition = "!is.null(output.lotto.selectInput)",
+      uiOutput("lotto.selectInput")
+    ),
+    conditionalPanel(
+      condition = "!is.null(output.ticket.selectInput)",
+      uiOutput("ticket.selectInput"),
+      uiOutput("system.selectInput"),
+      uiOutput("number.selectInput")
+    ),
     
-    sliderInput( "ticketNum", "Number of tickets to generate:", min=1, max=20, value=12),
-    
-    selectInput( "systemChr", "Choose a System:",
-                 choices=c("NA", "7", "8", "9", "10", "11", "12") )
-    
+    submitButton("Ok")
   ),
   
   # Show a summary of the dataset and an HTML table with the requested
@@ -34,7 +39,7 @@ shinyUI(pageWithSidebar(
     tabsetPanel(
       tabPanel("Tickets", h4("Good Luck!"), verbatimTextOutput("ticketTxt") ), 
       tabPanel("Confidence", h4("Interval"), verbatimTextOutput("intervalTxt") ), 
-      tabPanel("Summary", verbatimTextOutput("summaryTxt") ),
+      tabPanel("Summary", h4("Power"), verbatimTextOutput("summaryTxt") ),
       tabPanel("Result", h4("Latest"), verbatimTextOutput("resultTxt") )
     )
   )
