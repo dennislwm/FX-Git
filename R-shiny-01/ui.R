9688766ef925b6d949c4be32c091cbbcf387f8d0
Dennis Lee <dennislwm@yahoo.com.au>
2012-12-03 17:13:34 +0800

    1.0.2   Modified script to work with a major revision of PlusLotto 1.0.7+. Added ONE (1) reactive UI ci.SelectInput.

--- R-shiny-01/ui.R
+++ R-shiny-01/ui.R
@@ -3,6 +3,8 @@
 #|                                                             Copyright © 2012, Dennis Lee |
 #|                                                                                          |
 #| Assert History                                                                           |
+#|  1.0.2   Modified script to work with a major revision of PlusLotto 1.0.7. Added ONE (1) |
+#|            reactive UI ci.SelectInput.                                                   |
 #|  1.0.1   Major bug fixes including interaction flow, output variables and reactiveUI.    |
 #|            Incorporated Roulette (r) into both server and ui.                            | 
 #|  1.0.0   This script contains the shinyUI() function for PlusLotto.R.                    |
@@ -21,7 +23,8 @@ shinyUI(pageWithSidebar(
   sidebarPanel(
     conditionalPanel(
       condition = "!is.null(output.lotto.selectInput)",
-      uiOutput("lotto.selectInput")
+      uiOutput("lotto.selectInput"),
+      uiOutput("ci.selectInput")
     ),
     conditionalPanel(
       condition = "!is.null(output.ticket.selectInput)",
