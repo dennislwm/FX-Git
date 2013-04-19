7da6f24466c5874087a967f5cff36c29d2c15fba
dennislwm <dennislwm@yahoo.com.au>
2013-04-19 12:08:13 +0800

    1.0.3   Renamed several functions from RegGetxxxDir() to RegxxxDir() to simplify names.                Changed name of function RegGetRDir() to RegGitDir().                Added functions RegBitDir(), RegProgramDir() and RegIsWindows64Bln().                Note: Due to name changes, this version is NOT backward compatible.

--- R-source/PlusReg.R
+++ R-source/PlusReg.R
@@ -4,6 +4,10 @@
 #| Assert Function                                                                          |
 #|                                                                                          |
 #| Assert History                                                                           |
+#|  1.0.3   Renamed several functions from RegGetxxxDir() to RegxxxDir() to simplify names. |
+#|          Changed name of function RegGetRDir() to RegGitDir().                           |
+#|          Added functions RegBitDir(), RegProgramDir() and RegIsWindows64Bln().           |
+#|          Note: Due to name changes, this version is NOT backward compatible.             |
 #|  1.0.2   Added function RegIsDateBln() to check for variables of type Date.              |
 #|  1.0.1   Fixed RegIsEmailBln() to check for repeating ".xxx" pattern.                    |
 #|  1.0.0   This library contains external R functions to perform text manipulation.        |
@@ -26,31 +30,55 @@ RegSystemNum <- function(cmdChr)
                         error=function(e) { 9999 }, finally={} )
   errNum
 }
-RegGetRSourceDir <- function()
+RegEaDir <- function()
 {
   retDir <- NULL
-  pathDir <- RegGetRDir()
+  pathDir <- RegGitDir()
+  if( !is.null(pathDir) )
+    retDir <- paste0(pathDir,"experts/")
+  retDir
+}
+RegJavaDir <- function()
+{
+  retDir <- NULL
+  pathDir <- RegGitDir()
+  if( !is.null(pathDir) )
+    retDir <- paste0(pathDir,"java/")
+  retDir
+}
+RegRSourceDir <- function()
+{
+  retDir <- NULL
+  pathDir <- RegGitDir()
   if( !is.null(pathDir) )
     retDir <- paste0(pathDir,"R-source/")
   retDir
 }
-RegGetRNonSourceDir <- function()
+RegRNonSourceDir <- function()
 {
   retDir <- NULL
-  pathDir <- RegGetRDir()
+  pathDir <- RegGitDir()
   if( !is.null(pathDir) )
     retDir <- paste0(pathDir,"R-nonsource/")
   retDir
 }
-RegGetRDir <- function()
+RegBitDir <- function()
 {
   retDir <- NULL
-  homeDir <- RegGetHomeDir()
+  homeDir <- RegHomeDir()
+  if( !is.null(homeDir) )
+    retDir <- paste0(homeDir,"100 FxOption/103 FxOptionVerBack/081 Fx Bit/")
+  retDir
+}
+RegGitDir <- function()
+{
+  retDir <- NULL
+  homeDir <- RegHomeDir()
   if( !is.null(homeDir) )
     retDir <- paste0(homeDir,"100 FxOption/103 FxOptionVerBack/080 Fx Git/")
   retDir
 }
-RegGetHomeDir <- function()
+RegHomeDir <- function()
 {
   retDir <- NULL
   if( RegIsLinuxBln() )
@@ -59,9 +87,24 @@ RegGetHomeDir <- function()
     retDir <- paste0("C:/Users/",Sys.info()["user"],"/")
   retDir
 }
+RegProgramDir <- function()
+{
+  retDir <- NULL
+  if( RegIsWindows64Bln() )
+    retDir <- "C:/Program Files (x86)/"
+  else if( RegIsWindowsBln() )
+    retDir <- "C:/Program Files/"
+  retDir
+}
 RegIsWindowsBln <- function()
 {
   retBln <- (Sys.info()["sysname"] == "Windows")
+  names(retBln) <- NULL
+  retBln
+}
+RegIsWindows64Bln <- function()
+{
+  retBln <- (length(grep("x64", Sys.info()["release"])) > 0) & RegIsWindowsBln()
   names(retBln) <- NULL
   retBln
 }
